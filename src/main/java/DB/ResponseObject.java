17022ca65727a3506eb3e60449e57d3450921299
s185120 <s185120@student.dtu.dk>
2020-05-07 00:26:10 +0200

    Lavet lidt mere på ResponseObject

--- src/main/java/DB/ResponseObject.java
+++ src/main/java/DB/ResponseObject.java
@@ -3,8 +3,68 @@ package DB;
 import java.util.ArrayList;
 
 public class ResponseObject {
+    private int errorCode;
     private String statusMessage;
-    private String response;
-    private ArrayList<String[]> asd;
+    private boolean success;
+    private String responseString;
+    private String[] responseStringArray;
+    private ArrayList<String[]> responseArraylist;
+
+    public ResponseObject(int errorCode, String statusMessage, boolean success, String responseString, String[] responseStringArray, ArrayList<String[]> responseArraylist) {
+        this.errorCode = errorCode;
+        this.statusMessage = statusMessage;
+        this.success = success;
+        this.responseString = responseString;
+        this.responseStringArray = responseStringArray;
+        this.responseArraylist = responseArraylist;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getResponseString() {
+        return responseString;
+    }
+
+    public void setResponseString(String responseString) {
+        this.responseString = responseString;
+    }
+
+    public String[] getResponseStringArray() {
+        return responseStringArray;
+    }
+
+    public void setResponseStringArray(String[] responseStringArray) {
+        this.responseStringArray = responseStringArray;
+    }
+
+    public ArrayList<String[]> getResponseArraylist() {
+        return responseArraylist;
+    }
+
+    public void setResponseArraylist(ArrayList<String[]> responseArraylist) {
+        this.responseArraylist = responseArraylist;
+    }
 
 }
